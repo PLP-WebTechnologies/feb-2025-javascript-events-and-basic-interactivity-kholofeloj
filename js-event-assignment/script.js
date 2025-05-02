6085524e742fe8074c86356bf61afb18d844e954
kholofeloj <kholofelojmocheko@gmail.com>
2025-05-02 22:06:51 +0200

    added slidesho functionlity

--- js-event-assignment/script.js
+++ js-event-assignment/script.js
@@ -18,3 +18,31 @@ const textOnClick = document.getElementById("change-color-onClick");
 button.onclick = () => {
     textOnClick.style.color = "#FF6B78";
 }
+
+
+// Slideshow
+let slideIndex = 0;
+
+const slides = document.querySelectorAll(".slide");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+const showSlide = (index) => {
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === index) ? "block" : "none";
+    });
+};
+
+const nextSlide = () => {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+};
+
+const prevSlide = () => {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide(slideIndex);
+};
+
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
+showSlide(slideIndex);
